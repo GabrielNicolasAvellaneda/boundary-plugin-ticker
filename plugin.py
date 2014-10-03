b515aa8f582b00d93aeb0a53f2c9956fec312746
jdgwartney <davidg@boundary.com>
2014-10-02 17:05:19 -0700

    Add support for multiple stocks

--- plugin.py
+++ plugin.py
@@ -2,17 +2,26 @@
 import ystockquote
 import sys
 import string
+import time
+import json
 
 """
 Implements a plugin that gets current stock price and volume
 """
 
-# We need to have two arguments the second being the ticker symbol
-if len(sys.argv) == 2:
-  # Make uppercase
-  ticker = sys.argv[1].upper()
-  price = ystockquote.get_price(ticker)
-  volume = ystockquote.get_volume(ticker)
-  if volume != 'N/A':
-    print('{0} {1} {2}'.format('BOUNDARY_STOCK_PRICE',price,ticker))
-    print('{0} {1} {2}'.format('BOUNDARY_STOCK_VOLUME',volume,ticker))
+if len(sys.argv) == 1:
+  pollInterval = 20
+
+json_data = open("param.json")
+data = json.load(json_data)
+# Loop over the items and lookup the stock price and volume
+items = data["items"]
+while True:
+  for i in items:
+    ticker = str(i["ticker"]).upper()
+    price = ystockquote.get_price(ticker)
+    volume = ystockquote.get_volume(ticker)
+    if volume != 'N/A':
+      print('{0} {1} {2}'.format('BOUNDARY_STOCK_PRICE',price,ticker))
+      print('{0} {1} {2}'.format('BOUNDARY_STOCK_VOLUME',volume,ticker))
+  time.sleep(pollInterval)
