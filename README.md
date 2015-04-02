bfc72459a2eb8d38e05a8b5c9d9e2b7545baf37e
jdgwartney <davidg@boundary.com>
2015-04-02 11:06:13 -0700

    Change plugin to a python class ; add boostrapping

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
-boundary-plugin-ticker
-======================
+Boundary Stock Ticker Plugin
+============================
 
-Plugin that provides the current stock price and volume of stock given its stock ticker.
+Example Meter Plugin that provides the current stock price and volume of stock given its stock ticker.
 
+### Prerequisites
 
-The Boundary Plugins Development Guide uses the plugin to describe the tips and best practices for developing Boundary Premium metric plugins.
-
-
-Prerequisites
--------------
-The following items are required on the host platform in addition to the Boundary Premium [relay](http://premium-support.boundary.com/customer/portal/articles/1635550-plugins---how-to).
-* Python 2.6.6 or later
-* Python [_ystockquote_](https://pypi.python.org/pypi/ystockquote) module
+|     OS    | Linux | Windows | SmartOS | OS X |
+|:----------|:-----:|:-------:|:-------:|:----:|
+| Supported |   v   |    v    |    v    |  v   |
 
 
-|Metric Name |Description                             |
-|------------|----------------------------------------|
-|Stock Price |Current stock price for the current day |
-|Stock Volume|Current stock volume for the current day|
+|  Runtime | node.js | Python | Java |
+|:---------|:-------:|:------:|:----:|
+| Required |         |   +    |      |
+
+
+- [How to install Python?](https://help.boundary.com/hc/articles/202270132)
+- Python [_ystockquote_](https://pypi.python.org/pypi/ystockquote) module
+
+
+|Metric Name  |Metric Identifier      |Description                             |
+|:------------|:----------------------|:---------------------------------------|
+|Stock Price  |BOUNDARY\_STOCK\_PRICE |Current stock price for the current day |
+|Stock Volume |BOUNDARY\_STOCK\_VOLUME|Current stock volume for the current day|
 
 ## Dashboards
 Here are the list of dashboards that are created when the ticker plugin is installed with their descriptions.
 
-|Dashboard| Description                                    |
-|---------|------------------------------------------------|
+|Dashboard|Description                                     |
+|:--------|:-----------------------------------------------|
 |Stocks   |Displays the current price and volume of a stock|
--- plugin.py
+++ plugin.py
@@ -1,28 +1,66 @@
 #/usr/bin/env python
+# Copyright 2014-2015 Boundary, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import ystockquote
 import sys
 import string
 import time
 import json
 
-"""
-Implements a plugin that gets current stock price and volume
-"""
+class Ticker():
+	"""Implements a meter plugin that gets current stock price and volume"""
+	def __init__(self, pollInterval):
+		self.pollInterval = pollInterval
 
-if len(sys.argv) == 1:
-  pollInterval = 10
+	def loadParameters(self):
+		""" Reads the meter plugin runtime parameters"""
+		json_data = open("param.json")
+		data = json.load(json_data)
+		self.items = data["items"]
 
-json_data = open("param.json")
-data = json.load(json_data)
-# Loop over the items and lookup the stock price and volume
-items = data["items"]
-while True:
-  for i in items:
-    ticker = str(i["ticker"]).upper()
-    price = ystockquote.get_price(ticker)
-    volume = ystockquote.get_volume(ticker)
-    if volume != 'N/A':
-      sys.stdout.write('{0} {1} {2}\n'.format('BOUNDARY_STOCK_PRICE',price,ticker).decode('utf-8'))
-      sys.stdout.write('{0} {1} {2}\n'.format('BOUNDARY_STOCK_VOLUME',volume,ticker).decode('utf-8'))
-      sys.stdout.flush()
-  time.sleep(pollInterval)
+	def sendEvent(self):
+		pass
+
+	def sendMeasurement(self,metric,value,source,timestamp):
+		""" Sends measurements to standard out to be read by plugin manager"""
+		sys.stdout.write('{0} {1} {2} {3}\n'.format(metric,value,source,timestamp).decode('utf-8'))
+		sys.stdout.flush()
+
+	def run(self):
+		"""Plugin main loop"""
+		self.loadParameters()
+		self.sendEvent()
+		while True:
+			# Loop over the items and lookup the stock price and volume
+			for i in self.items:
+				ticker = str(i["ticker"]).upper()
+    			price = ystockquote.get_price(ticker)
+    			volume = ystockquote.get_volume(ticker)
+    			if volume == 'N/A' or price == 'N/A':
+    				self.sendEvent()
+    				sys.exit(1)
+    			else:
+    				timestamp = int(time.time())
+    				self.sendMeasurement('BOUNDARY_STOCK_PRICE',price,ticker,timestamp)
+    				self.sendMeasurement('BOUNDARY_STOCK_VOLUME',volume,ticker,timestamp)
+				time.sleep(self.pollInterval)
+
+if __name__ == "__main__":
+    plugin = Ticker(10)
+    plugin.run()
+
+
+
+
+
